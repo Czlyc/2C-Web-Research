abbeff856afc6b88255cad990dae0e8aeaccf1c1
hpl <hupili@ie.cuhk.edu.hk>
2011-12-12 16:35:36 +0800

    add basic files

---
+++ proximity-random-walk/pagerank.h
@@ -0,0 +1,15 @@
+#ifndef __H_PAGERANK_
+#define __H_PAGERANK_
+
+#include <stdio.h>
+#include <string>
+#include <list>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+//deprecated use 
+//I just keep it simple this time
+using namespace std ;
+
+#endif
