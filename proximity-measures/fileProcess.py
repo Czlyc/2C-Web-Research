5b662120f5d39f99b2a66f450cde532620323288
Elvis Li <toczlyc@gmail.com>
2011-12-13 18:24:32 +0800

    add 2 input process function to handle label file and 1 output function to handle quality-functions outputs

--- proximity-measures/fileProcess.py
+++ proximity-measures/fileProcess.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python
 
+def countLine(inputFileName):
+    count=0
+    fid = open(inputFileName, 'r')
+    for eachline in fid:
+        eachline = eachline[:-1]
+        if eachline != '':
+            count+=1
+    fid.close()
+               
+    return count
+
+
 def deal_input(inputFileName):
     N = dict()
     fid = open(inputFileName, 'r')
@@ -24,7 +36,52 @@ def deal_input(inputFileName):
     return N, amLines
 
 def deal_output(amLines, someN, outputFileName):
-    fid=open(outputFileName, 'w')
-    for index,j in enumerate(amLines):
-        fid.write(str(j)+'\t'+str(someN[index])+'\n')
+    fid = open(outputFileName, 'w')
+    for index, j in enumerate(amLines):
+        fid.write(str(j) + '\t' + str(someN[index]) + '\n')
     fid.close()
+
+def deal_output2(qualDict, outputFileName):
+    fid = open(outputFileName, 'w')
+    for qualName in qualDict.keys():
+        fid.write(qualName + '\t' + str(qualDict[qualName]) + '\n')
+    fid.close()
+
+#Input: File in "NodeID /t Label" format
+#Output: C={LabelID:[NodeID1,NodeID2...]}
+def deal_input2(inputFileName):
+    C = dict()
+    fid = open(inputFileName, 'r')
+    for eachline in fid:
+        eachline = eachline[:-1]
+        if eachline != '':
+            arr = eachline.split('\t')
+            arr1 = int(arr[0])
+            arr2 = int(arr[1])
+            #Construct Class Label dict
+            if C.has_key(arr2):
+                C[arr2] += [arr1]
+            else:
+                C[arr2] = [arr1]
+    fid.close()
+              
+    return C
+
+
+#Input: File in "NodeID /t Label" format
+#Output: NC={NodeID1:LabelID , NodeID2:LabelID...}
+def deal_input3(inputFileName):
+    NC = dict()
+    fid = open(inputFileName, 'r')
+    for eachline in fid:
+        eachline = eachline[:-1]
+        if eachline != '':
+            arr = eachline.split('\t')
+            arr1 = int(arr[0])
+            arr2 = int(arr[1])
+            #Construct NodeID:Class Label dict
+            NC[arr1] = arr2
+            
+    fid.close()
+              
+    return NC
