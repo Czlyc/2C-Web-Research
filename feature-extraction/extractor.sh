8fb5d0c68ff97528861a425b90b8d96faef16823
hpl <hpl@moving.(none)>
2011-12-13 22:33:28 +0800

    extractor

--- feature-extraction/extractor.sh
+++ feature-extraction/extractor.sh
@@ -19,6 +19,7 @@ exe_pr="../proximity-random-walk/pagerank"
 exe_es_all1="../proximity-random-walk/gen-escape-all-one.sh"
 exe_es_high="../proximity-random-walk/gen-escape-high-degree-training.sh"
 exe_es_root="../proximity-random-walk/gen-escape-root-one.sh"
+exe_bi="../data/bidirectional.sh"
 
 dir_simple="../proximity-measures"
 exe_adamic="python ./adamicS.py"
@@ -29,12 +30,13 @@ rm -rf tmp
 mkdir -p tmp
 
 #pagerank
-#$exe_es_all1 $dir_raw > tmp/escape.all1
-#$exe_pr $dir_raw/link tmp/escape.all1 tmp/pr.all1 0.9
-#$exe_es_high $dir_raw > tmp/escape.high
-#$exe_pr $dir_raw/link tmp/escape.high tmp/pr.high 0.9
-#$exe_es_root $dir_raw > tmp/escape.root
-#$exe_pr $dir_raw/link tmp/escape.root tmp/pr.root 0.9
+cat $dir_raw/link | $exe_bi > tmp/link.bi
+$exe_es_all1 $dir_raw > tmp/escape.all1
+$exe_pr tmp/link.bi tmp/escape.all1 tmp/pr.all1 0.9
+$exe_es_high $dir_raw > tmp/escape.high
+$exe_pr tmp/link.bi tmp/escape.high tmp/pr.high 0.9
+$exe_es_root $dir_raw > tmp/escape.root
+$exe_pr tmp/link.bi tmp/escape.root tmp/pr.root 0.9
 
 rootid=`echo -n \` cat $dir_raw/rootid \``
 
@@ -42,11 +44,22 @@ rootid=`echo -n \` cat $dir_raw/rootid \``
 cd $dir_simple
 $exe_adamic $dir_raw/link $rootid $dir_cur/tmp/adamic
 cd -
+#common neighbour
 cd $dir_simple
 $exe_common $dir_raw/link $rootid $dir_cur/tmp/common
 cd -
+#jaccard's
 cd $dir_simple
 $exe_jacc $dir_raw/link $rootid $dir_cur/tmp/jacc
 cd -
+
+for file in "pr.all1" "pr.root" "pr.high" "common" "adamic" "jacc"
+do
+	sort -k1 tmp/$file > tmp/$file.s
+done
+
+paste "tmp/pr.all1.s" "tmp/pr.root.s" "tmp/pr.high.s" "tmp/common.s" "tmp/adamic.s" "tmp/jacc.s" > tmp/feature.merge
+
+awk -F"\t" '{OFS="\t";print $1,$2,$4,$6,$8,$10,$12}' tmp/feature.merge > $dir_raw/feature
 
 exit 0 
