cfc4cfd1b23420d8eccc9aba2106cee382210b47
hpl <hpl@moving.(none)>
2011-12-17 23:01:10 +0800

    add roc script

---
+++ roc/roc.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl -w 
+#hupili
+#20111217
+
+#draw the roc curve for single features. 
+#input the weka compatible .arff file. 
+
+use strict ;
+
+my $usage = "$0 {fn_arff}" ;
+
+my $ARGC = @ARGV ;
+($ARGC == 1) or die("usage:$usage\n") ;
+
+
+exit 0 ;
