700cacf600433feb7fa519a76519339166a015ae
hpl <hpl@moving.(none)>
2011-12-18 11:52:18 +0800

    plot suite

--- roc/run-roc.sh
+++ roc/run-roc.sh
@@ -10,7 +10,7 @@ for i in \
 	"$tmp/pr.all1.super" "$tmp/pr.root.super" "$tmp/pr.high.super" 
 do
 	echo $i
-	name=`echo -n $i | sed -e 's/tmp.*\///g' -e 's/\./_/g'`
+	name=`echo -n $i | sed -e 's/tmp.*\///g' -e 's/\./-/g'`
 	echo $name
 
 	./roc.pl $raw $i > $output/$name
