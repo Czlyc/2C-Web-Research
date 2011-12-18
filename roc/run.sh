a1ea660a6614fefc10f42ba7bd0af5d608bf4c11
hpl <hupili@ie.cuhk.edu.hk>
2011-12-18 10:42:30 +0800

    overall entrance of calculating roc

---
+++ roc/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+tmp="tmp.2" 
+raw="raw.2"
+output="o2"
+
+for i in \
+ "$tmp/pr.all1" "$tmp/pr.root" \
+	"$tmp/pr.high" "$tmp/common" "$tmp/adamic" "$tmp/jacc"  \
+	"$tmp/pr.all1.super" "$tmp/pr.root.super" "$tmp/pr.high.super" 
+do
+	echo $i
+	name=`echo -n $i | sed -e 's/tmp.*\///g' -e 's/\./_/g'`
+	echo $name
+
+	./roc.pl $raw $i > $output/$name
+done
